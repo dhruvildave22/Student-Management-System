26d1152f48be6724d55f49b30bb958fd73230423
dhruvildave <dhruvildave22@gmail.com>
2019-05-27 18:15:26 +0530

    helpers updated

--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -16,6 +16,24 @@ module AnalyticsHelper
     ).hexdigest
     }?f=y=#{size}&r=g\" style=\"height:#{size}px;width:#{size}px;border:#{border}px\">".html_safe
   end
+
+  def my_form_tag(get_students_by_course, &block)
+    attrs  = "method='get' action='#{get_students_by_course}'"
+    fields = capture(&block)
+    "<form #{attrs}>  #{fields} </form>".html_safe
+  end
+
+  def my_label_tag(course_type)
+    "<label>#{course_type}</label>".html_safe
+  end
+
+  def my_text_field_tag(name)
+    "<input name='#{:course_type}'  type='text' />".html_safe
+  end
+
+  def my_submit_tag(txt="Submit")
+    "<input type='submit' value='#{txt}'>".html_safe
+  end
 end
 
 # 
