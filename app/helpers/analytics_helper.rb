19030a8465d8f1f6831674217ea8c512ce84158b
dhruvildave <dhruvildave22@gmail.com>
2019-05-28 12:54:40 +0530

    helpers update

--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -17,23 +17,47 @@ module AnalyticsHelper
     }?f=y=#{size}&r=g\" style=\"height:#{size}px;width:#{size}px;border:#{border}px\">".html_safe
   end
 
-  def my_form_tag(get_students_by_course, &block)
-    attrs  = "method='get' action='#{get_students_by_course}'"
-    fields = capture(&block)
-    "<form #{attrs}>  #{fields} </form>".html_safe
+  def std_names(course_type, contrl, action, url )
+    if controller_name == contrl && action_name == action
+      return link_to course_type 
+    else
+      return link_to course_type, url
+    end
   end
 
-  def my_label_tag(course_type)
-    "<label>#{course_type}</label>".html_safe
-  end
+  # def my_form_tag(get_students_by_course, &searchbox)
+  #   attrs  = "method='get' action='#{get_students_by_course}'"
+  #   fields = capture(&searchbox)
+  #   "<form #{attrs}>  #{fields} </form>".html_safe
+  # end
+  # def my_form_tag(get_students_by_course, &searchbox)  
+  #   fields = capture(&searchbox)
+  #   "<form> #{fields}  </form>".html_safe
+  # end
 
-  def my_text_field_tag(name)
-    "<input name='#{:course_type}'  type='text' />".html_safe
-  end
+  # def current_user(all_students, &block)
+  #   attrs = student.find_by_course_type(params[:course_type])
+  #   fields = capture(&searchbox)
+  #  "<form #{attrs}>  #{fields} </form>".html_safe
+  # end
 
-  def my_submit_tag(txt="Submit")
-    "<input type='submit' value='#{txt}'>".html_safe
-  end
+
+  # def my_label_tag(course_type)
+  #   "<label>#{course_type}</label>".html_safe
+  # end
+
+  # def my_text_field_tag(name)
+  #   "<input name='#{:course_type}'  type='text' />".html_safe
+  # end
+
+  # def my_submit_tag(txt="Submit")
+  #   "<input type='submit' value='#{txt}'>".html_safe
+  # end
+  # def get_students(student)
+  #   if course_type = "RBSE"
+  #     student.@all_students(params[:course_type])
+  #   end
+
+
+  # end
 end
-
-# 
