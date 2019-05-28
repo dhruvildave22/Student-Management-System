51d1cd28dd2b55cfcdc1868cc4b62c0ca9a66d10
dhruvildave <dhruvildave22@gmail.com>
2019-05-28 17:36:44 +0530

    statement correction done 2

--- app/controllers/concerns/studentable.rb
+++ app/controllers/concerns/studentable.rb
@@ -2,10 +2,10 @@ module Studentable
   extend ActiveSupport::Concern
   
   def find_all_students(subject_name)
-    s1 = Subject.find_by_subject_name(params[:subject_name])
+    s1 = Subject.find_by_subject_name(subject_name)
+    get_student = []
     get_teacher = s1.teachers
     get_teacher.each do |t|
-      get_student = []
       get_student.push(t.students)
     end
   end
