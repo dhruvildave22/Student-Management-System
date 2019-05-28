856e46ed418ab7ea90c20b0e61af7a404a2c2169
dhruvildave <dhruvildave22@gmail.com>
2019-05-28 17:26:48 +0530

    array indentation resolved

--- app/controllers/concerns/studentable.rb
+++ app/controllers/concerns/studentable.rb
@@ -3,8 +3,8 @@ module Studentable
   
   def find_all_students(subject_name)
     s1 = Subject.find_by(subject_name:params[:subject_name])
-    get_teacher = s1.teachers
     get_student = []
+    get_teacher = s1.teachers
     get_teacher.each do |t|
       get_student.push(t.students)
     end
