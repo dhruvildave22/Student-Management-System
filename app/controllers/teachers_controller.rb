fd1d735b68bee839f05b967ddb634dfb145d81c0
dhruvildave <dhruvildave22@gmail.com>
2019-05-29 13:11:48 +0530

    exception handler added forteachers

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,17 +1,21 @@
 class TeachersController < ApplicationController
   skip_before_action :verify_authenticity_token
+  before_action :set_teacher, only: [:show, :update, :edit, :destroy]  
 
-  def new
-    @teacher = Teacher.new
+    def index
+    @teacher = Teacher.all
     respond_to do |format|
       format.json { render json: { teacher: @teacher }, status: :ok }
     end
   end
 
   def show
-    @teacher = Teacher.find(params[:id])
     respond_to do |format|
       format.json { render json: { teacher: @teacher }, status: :ok }
+    end
+  rescue ActiveRecord::RecordNotFound => e
+    respond_to do |format|
+      format.json { render json: { error: e.message }, status: :not_found }
     end
   end
 
@@ -19,30 +23,53 @@ class TeachersController < ApplicationController
     @teacher = Teacher.new(teacher_params)
     respond_to do |format|
       if @teacher.save
-        format.html { redirect_to school_path(@teacher.school) }
         format.json { render json: { teacher: @teacher }, status: :created }
+      else
+        format.json { render json: @teacher.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  def index
-    @teachers = Teacher.all
-    render json: @teachers
-  end
-
-  def edit
-    @teacher = Teacher.find(params[:id])
+  def update
     respond_to do |format|
-      format.json { render json: { teacher: @teacher }, status: :ok }
+      if @teacher.update(teacher_params)
+        format.json { render json: { teacher: @teacher }, status: :ok }
+      else
+        format.json { render json: @teacher.errors, status: :unprocessable_entity }
+      end
+    end
+  rescue StandardError => e
+    respond_to do |format|
+      format.json { render json: { error: e.message }, status: :unprocessable_entity }
     end
   end
 
+  def edit
+    respond_to do |format|
+      format.json { render json: { teacher: @teacher }, status: :ok }
+    end
+  rescue ActiveRecord::RecordNotFound => e
+    respond_to do |format|
+      format.json { render json: { error: e.message }, status: :not_found }
+    end
+  end
+
+  def destroy
+    respond_to do |format|
+      @teacher.destroy
+      format.json { render json: {}, status: :ok }
+    end
+  rescue ActiveRecord::RecordNotFound => e
+    respond_to do |format|
+      format.json { render json: { error: e.message }, status: :unprocessable_entity }
+    end
+  end
   private
   def set_teacher
     @teacher = Teacher.find(params[:id])
   end
 
   def teacher_params
-    params.require(:teacher).permit(:t_name, :father_name, :gender, :date_of_birth, :t_address, :date_of_join, :school_id, :student, :teacherable_id, :teacherable_type)
+    params.require(:teacher).permit(:t_name, :father_name, :gender, :date_of_birth, :t_address, :date_of_join, :teacher_id, :student, :teacherable_id, :teacherable_type)
   end
 end
