2f4ea7b70d4401d1187760e0c43907cd7659b63d
dhruvildave <dhruvildave22@gmail.com>
2019-05-31 11:28:36 +0530

    transactions done 1

--- app/controllers/partials_controller.rb
+++ app/controllers/partials_controller.rb
@@ -6,14 +6,18 @@ class PartialsController < ApplicationController
     if school.school_code == params[:school_code]
       School.transaction do 
         school.update_attributes!(:school_code => params[:school_code])        
-        teacher = Teacher.first
-        teacher.update_attributes!(:teacher_code => params[:teacher_code]) 
-        student = Student.first
-        student.update_attributes!(:student_code => params[:student_code])  
+        teacher = Teacher.find_by_id(params[:teacher_id])
+        teacher.update_attributes!(:teacher_code => params[:teacher_code])
+      rescue ActiveRecord::RecordNotFound => e
+        render json: { error: e.message }, status: :unprocessable_entity 
+        student = Student.find_by_id(params[:student_id])
+        student.update_attributes!(:student_code => params[:student_code])
+      rescue ActiveRecord::RecordNotFound => e
+        render json: { error: e.message }, status: :unprocessable_entity  
       end
+    else
+      raise ActiveRecord::Rollback
     end
-  rescue => e
-    p e
   end
 end
 
