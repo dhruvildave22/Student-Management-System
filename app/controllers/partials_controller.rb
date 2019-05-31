5244875abf1d4bd8df7bf7534c9b22310f2f29c1
dhruvildave <dhruvildave22@gmail.com>
2019-05-31 17:18:51 +0530

    exception added

--- app/controllers/partials_controller.rb
+++ app/controllers/partials_controller.rb
@@ -5,13 +5,13 @@ class PartialsController < ApplicationController
     school = School.find_by_id(params[:id])
     if school.school_code != params[:school_code]
       School.transaction do 
-        school.update_attributes!(:school_code => params[:school_code])        
+        school.update_attributes!(:school_code => school.school_code)        
         teacher = Teacher.find_by_id(params[:teacher_id])
         teacher.update_attributes!(:teacher_code => school.school_code)
         student = Student.find_by_id(params[:student_id])
         student.update_attributes!(:student_code => school.school_code)
-      rescue ActiveRecord::RecordNotFound => e
-        render json: { error: e.message }, status: :unprocessable_entity  
+      rescue NoMethodError => e
+        render json: { error: 'data not found'}, status: :internal_server_error    
       end
     end
   end
