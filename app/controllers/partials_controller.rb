243afb860fed3ae7abc87b7cd260b8c39649adac
dhruvildave <dhruvildave22@gmail.com>
2019-06-02 21:38:38 +0530

    transaction handled

--- app/controllers/partials_controller.rb
+++ app/controllers/partials_controller.rb
@@ -2,17 +2,20 @@ class PartialsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def update_school_and_teacher_code
-    school = School.find_by_id(params[:id])
+    school = School.find(params[:id])
     if school.school_code != params[:school_code]
       School.transaction do 
-        school.update_attributes!(:school_code => school.school_code)        
-        teacher = Teacher.find_by_id(params[:teacher_id])
+        school.update_attributes!(:school_code => params[:school_code])        
+        teacher = Teacher.find(params[:teacher_id])
         teacher.update_attributes!(:teacher_code => school.school_code)
-        student = Student.find_by_id(params[:student_id])
-        student.update_attributes!(:student_code => school.school_code)
-      rescue NoMethodError => e
-        render json: { error: 'data not found'}, status: :internal_server_error    
+        student = Student.find(params[:student_id])
+        student.update_attributes!(:student_code => school.school_code) 
+        render json: { message: ' data update_school_and_teacher_code '}
       end
+    else
+      render json: { message: ' data not updated '}
     end
+    rescue => e
+      render json: { error: ' data not found '}
   end
 end
