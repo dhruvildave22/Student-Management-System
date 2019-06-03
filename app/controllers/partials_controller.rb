be8f7d696485c871ced4d600c734e87c35b575a2
dhruvildave <dhruvildave22@gmail.com>
2019-06-03 15:13:21 +0530

    transaction done

--- app/controllers/partials_controller.rb
+++ app/controllers/partials_controller.rb
@@ -5,18 +5,15 @@ class PartialsController < ApplicationController
     school = School.find(params[:id])
     if school.school_code != params[:school_code]
       School.transaction do 
-        school.update_attributes!(:school_code => params[:school_code])        
+        school.update_attributes!(:school_code => params[:school_code]) 
         teacher = Teacher.find(params[:teacher_id])
         teacher.update_attributes!(:teacher_code => school.school_code)
         student = Student.find(params[:student_id])
         student.update_attributes!(:student_code => school.school_code) 
-        render json: { message: ' data update_school_and_teacher_code '}
       end
-    else
-      render json: { message: ' data not updated '}
+      render json: { message: ' data update_school_and_teacher_code '}
     end
     rescue => e
       render json: { message: ' data not found '}
   end
 end
-  
